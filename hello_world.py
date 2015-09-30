4e81edc3ba49358a7f6dde037467a643d152165f
Sam Dillard <samhdillard@gmail.com>
2015-09-30 02:24:18 +0000

    initial commit on Heroku deployment

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from os import environ
 
 app = Flask(__name__)
@@ -10,7 +10,11 @@ def say_hi():
     
 @app.route("/hello/<name>")
 def hi_person(name):
-    return "Hello {}!".format(name.title())
+    return render_template('template.html', name=name.title())
+    
+@app.route("/hello/<firstname>/<lastname>")
+def hi_jedi(firstname,lastname):
+    return render_template('jedi.html', lastname=lastname[0:3].title(),firstname=firstname[0:2])
 
 if __name__ == "__main__":
     app.run(host=environ['IP'],
